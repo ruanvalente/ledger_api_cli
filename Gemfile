48930a7f8fc7a60837b32038d0fd30fa0863ebe8
ruanvalente <ruanopensuselinux@gmail.com>
2025-03-15 14:17:40 -0300

    feat: add Gemfile and database schema for transactions

---
+++ Gemfile
@@ -0,0 +1,2 @@
+gem 'sqlite3', '~> 1.3', '>= 1.3.11'
+gem 'terminal-table'
---
+++ lib/database/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS transactions (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  value REAL NOT NULL,
+  date TEXT NOT NULL,
+  description TEXT
+);
